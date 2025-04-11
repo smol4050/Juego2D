315801c6973c714301f0099dfccdf10e99eb81d1
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-04-11 00:22:28 -0500

    finalizacion transicion
    kjhbj

---
+++ Videojuego 2D/Assets/AssetsMilo/Scripts_Taller/Transcicion_Controller.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionController : MonoBehaviour
+{
+    public static TransitionController Instance;
+
+    private Animator animator;
+    [SerializeField] private AnimationClip animacionFinal;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public void CambiarEscena(string nombreEscena)
+    {
+        Debug.Log("Cambiando a la escena: " + nombreEscena);
+        StartCoroutine(Transicion(nombreEscena));
+    }
+
+    private IEnumerator Transicion(string nombreEscena)
+    {
+
+        Debug.Log("Iniciando la transición a la escena: " + nombreEscena);
+
+        animator.SetTrigger("TransicionActiva"); // activa la animación
+
+        yield return new WaitForSeconds(animacionFinal.length);
+        SceneManager.LoadScene(nombreEscena);
+
+        
+    }
+}
