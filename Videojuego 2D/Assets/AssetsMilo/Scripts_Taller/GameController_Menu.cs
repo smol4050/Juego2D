2d750bd0dde08dfad5fc66504c8cec77c9d03c1a
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-04-11 10:33:49 -0500

    MenuFinalizadisimo
     edvgr

--- Videojuego 2D/Assets/AssetsMilo/Scripts_Taller/GameController_Menu.cs
+++ Videojuego 2D/Assets/AssetsMilo/Scripts_Taller/GameController_Menu.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 #if UNITY_EDITOR
 using UnityEditor; // Importante para detener la ejecución en el editor
 #endif
 
-public class ExitProgram : MonoBehaviour
+public class GameController_Menu : MonoBehaviour
 {
+    public void IniciarJuego(string nameScene)
+    {
+        TransitionController.Instance.CambiarEscena(nameScene);
+    }
+
+    public void MostrarCreditos(string nameScene)
+    {
+        Debug.Log("Mostrar Creditos");
+        TransitionController.Instance.CambiarEscena(nameScene);
+    }
+
     public void Exit()
     {
         Debug.Log("Exit");
