e639f27f8b83ab57b33b4cf05e95a1e4c1f26102
Alejandro Quintero Beltrán <alejandroquinterobeltran9@gmail.com>
2025-04-12 00:17:56 -0500

    sonidos del enemigo

---
+++ Videojuego 2D/Assets/Scripts/controladorSonidoEnemigo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorSonidoEnemigo : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] DamageReceived;
+    [SerializeField] private AudioClip Died;
+    public AudioSource audioSource;
+    public void selectAudioDied()
+    {
+       Debug.Log("Se intentó reproducir sonido de muerte del enemigo.");
+        if (audioSource == null)
+        {
+         Debug.LogError("audioSource no está asignado en controladorSonidoEnemigo.");
+        }
+        audioSource.PlayOneShot(Died);
+    }
+    public void selectAudioDamageReceived()
+    {
+        int sonidoElegido = Random.Range(0, DamageReceived.Length);
+        audioSource.PlayOneShot(DamageReceived[sonidoElegido]);
+    }
+
+    
+}
