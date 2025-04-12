f25b735b1832cd25eda4f9ac8733997a389b6de6
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-04-12 09:04:58 -0500

    cracion estrellacambio nivel
    kjnk

---
+++ Videojuego 2D/Assets/AssetsMilo/Scripts_Taller/GameController_Steven.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController_Steven : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI txtGeneralScore;
+    [SerializeField] public int itemScore = 0;
+
+
+    public void Update()
+    {
+        txtGeneralScore.text = GameManager_Taller.Instance.GetScore() + "";
+    }
+
+    public void SumValues(int count)
+    {
+        itemScore += count;
+        GameManager_Taller.Instance.AgregarPuntaje(count);
+
+    }
+
+    public void itemsRecolectados(int item)
+    {
+        GameManager_Taller.Instance.AgregarElementosRecolectados(item);
+    }
+
+    public void siguienteNivel()
+    {
+        TransitionController.Instance.CambiarEscena("Milo");
+    }
+}
